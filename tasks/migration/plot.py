7ccfe0268e7557be2c670b43d5c58327134f0e0c
Eleftheria Mappoura <e.mappoura22@imperial.ac.uk>
2025-02-14 16:11:03 +0000

    migration:add grid + latex font to plot

--- tasks/migration/plot.py
+++ tasks/migration/plot.py
@@ -1,6 +1,7 @@
 from glob import glob
 from invoke import task
 from matplotlib.pyplot import hlines, subplots
+import matplotlib.pyplot as plt
 from numpy import arange
 from os.path import join
 from pandas import read_csv
@@ -17,6 +18,16 @@ ALL_WORKLOADS = [
     "very-network",
 ]
 
+plt.rcParams.update({
+    "text.usetex": True,            # Use LaTeX for all text
+    "font.family": "serif",         # Use serif fonts (like LaTeX default)
+    "font.serif": ["Times"],        # Use Times font (matches ACM's acmart class default)
+    "axes.labelsize": 12,           # Font size for axes labels
+    "font.size": 12,                # General font size
+    "legend.fontsize": 12,          # Font size for legend
+    "xtick.labelsize": 10,          # Font size for x tick labels
+    "ytick.labelsize": 10           # Font size for y tick labels
+})
 
 def _read_results():
     results_dir = join(PROJ_ROOT, "results", "migration")
@@ -81,11 +92,12 @@ def do_plot(workload, migration_results):
         label=workload,
         color=UBENCH_PLOT_COLORS[color_idx],
         edgecolor="black",
+        zorder = 3
     )
 
     # Aesthetics
     ax.set_ylabel("Speed-up \n [No mig. / mig.]")
-    ax.set_xlabel("% of execution when to migrate")
+    ax.set_xlabel(r'\% of execution when to migrate')
     ax.set_xticks(xticks)
     ax.set_xticklabels(["1 VM", "20", "40", "60", "80"])
     xlim_left = 0.5
@@ -108,8 +120,8 @@ def do_plot(workload, migration_results):
         ax.set_ylim(bottom=0, top=6)
     else:
         ax.set_ylim(bottom=0)
-
-    hlines(1, xlim_left, xlim_right, linestyle="dashed", colors="red")
+    ax.grid(zorder=0)
+    hlines(1, xlim_left, xlim_right, linestyle="dashed", colors="red", zorder=4)
 
     save_plot(
         fig, MIGRATION_PLOTS_DIR, "migration_speedup_{}".format(workload)
